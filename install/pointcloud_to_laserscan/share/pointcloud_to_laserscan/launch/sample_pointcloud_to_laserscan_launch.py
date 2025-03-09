29b129c069ca5cca439e0b81c01eec1e5cb05303
AlexReis1313 <alexandreleitereis@gmail.com>
2025-03-09 17:57:24 +0100

    implemented lidar2scan filter

--- install/pointcloud_to_laserscan/share/pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
+++ install/pointcloud_to_laserscan/share/pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
@@ -55,21 +55,23 @@ def generate_launch_description():
             remappings=[('cloud_in', 'ouster/points_corrected'),
                         ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan/merged'])],
             parameters=[{
-                'target_frame': 'cloud',
+                'target_frame': 'laser_scan_frame',
+                'fixed_frame': 'map',
+                'cloud_frame': 'os_sensor',             
                 'transform_tolerance': 0.01,
-                'min_height_shortrange': -15,
-                'max_height_shortrange': 15.0,
-                'min_height_longrange_': -15.0,
-                'max_height_longrange_': 15.0,
+                'min_height_longrange': -8.0,
+                'max_height_longrange': 8.0,
                 'angle_min': -3.14159,  # -M_PI/2
                 'angle_max': 3.14159,  # M_PI/2
                 'angle_increment': 0.003067,  # M_PI/360.0
                 'scan_time': 0.1,
                 'range_min': 1.5,
-                'range_transition': 5.0,
-                'range_max': 300.0,
+                'range_max': 5000.0,
                 'use_inf': True,
-                'inf_epsilon': 1.0
+                'inf_epsilon': 1.0,
+                'min_height_shortrange': -0.2,
+                'max_height_shortrange': 6.0,
+                'range_transition': 20.0
             }],
             name='pointcloud_to_laserscan_merged'
         )
--- new_clone/build/perception_pcl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/thesis_ws/new_clone/src/perception_pcl/perception_pcl
-# Build directory: /home/alex/thesis_ws/new_clone/build/perception_pcl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
